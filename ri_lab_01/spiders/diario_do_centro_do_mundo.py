53c32c3c38e350b4f3053fbfc16bad0c62003bbb
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2019-04-05 23:20:05 -0300

    Refactor and fix date

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -16,36 +17,34 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
         with open('seeds/diario_do_centro_do_mundo.json') as json_file:
                 data = json.load(json_file)
         self.start_urls = list(data.values())
-        # print('*************************************************')
-        # print(self.start_urls)
-        # print('*************************************************')
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
         for href in response.css("div.td-block-span4 a::attr(href)"):
                 yield response.follow(href, self.parse_post, meta={'url': response.url})
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        
     def parse_post(self, response):
-        def extract_with_css(query):
-            return response.css(query).get(default='').strip()
-        print('\n')
+
+        def extract_generic(selector):
+            return response.css(selector).get()
+
+        def extract_text(selector):
+            result = response.css(selector).getall()
+            return ' '.join(result)
+        
+        def extract_section():
+            return response.meta['url'].split('/')[-2]
+
+        def extract_date(selector):
+            date = extract_generic(selector)
+            dt = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
+            return dt.strftime('%d/%m/%Y %H:%M:%S')
+
         yield {
-            'title': extract_with_css('h1.entry-title::text'),
-            'author': extract_with_css('div.td-author-by + a::text'),
-            'date': extract_with_css('time::text'),
-            'url': response.url,
-            'section': response.meta['url'].split('/')[-2],
-            'text': response.xpath('//div[contains(@class, "td-post-content")]//p/text() | \
-                                    //div[contains(@class, "td-post-content")]//p/span/text()').getall()#getall(default='').strip(),
-            # response.xpath("//p/descendant::text()[not(parent::div/@class='td_block_template_1')]").getall()
+            'title': extract_generic('h1.entry-title::text'),
+            'sub_title': '',
+            'author': extract_generic('div.td-author-by + a::text'),
+            'date': extract_date('time::attr(datetime)'),
+            'section': extract_section(),
+            'text': extract_text('.p2::text, .s1::text, p:not(.donation_paragraph)::text'),
+            'url': response.url
         }
-        print('\n')
